dd02cc730660cb38dfdd1b575ca870a39fcb16eb
AdityaKumar5155 <adityakumar5155@gmail.com>
2023-03-13 02:21:59 +0530

    In progress

--- 1.js
+++ 1.js
@@ -4,9 +4,12 @@ const server = require('http').Server(app);
 const {v4:uuidv4} = require('uuid');
 const io = require('socket.io')(server);
 const port = 4444;
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {debug : true});
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
+app.use('/peerjs', peerServer);
 app.get('/', (req, res) => {
     res.redirect(`/${uuidv4()}`);
 })
@@ -16,8 +19,9 @@ app.get('/:room', (req, res) => {
 })
 
 io.on('connection', socket => {
-    socket.on('join-room',() => {
-        console.log('joined');
+    socket.on('join-room',(roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
     })
 })
 
--- public/script.js
+++ public/script.js
@@ -3,18 +3,51 @@ let myVideoStream;
 const video_grid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 // myVideo.muted = false;
+var peer = new Peer(undefined, {
+    path : '/peerjs',
+    host : '/',
+    port : '4444'
+});
+
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+
+    peer.on('call', call => {
+        call.answer(stream);
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+
+    socket.on('user-connected', (userId) => {
+        connecToNewUser(userId, stream);
+    })
 })
-socket.emit('join-room');
+peer.on('open', (id) => {
+    socket.emit('join-room', ROOM_ID , id);
+    console.log(id);
+})
+
+
+
+
+const connecToNewUser = (userId, stream) => {
+    const call = peer.call(userId,stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
+}
+
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
     video.addEventListener('loadedmetadata', () => {
-        video.play();
+        video.play(); 
     })
     video_grid.appendChild(video);
 }
